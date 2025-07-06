c362614e58ccc3518f08a80db749d8f8af0f7140
FloAvis <152602773+FloAvis@users.noreply.github.com>
2025-07-06 16:22:04 +0200

    More debugging and Onehot vs internal

--- OneHot_vs_internal.py
+++ OneHot_vs_internal.py
@@ -174,7 +174,7 @@ def main():
     files = [r"data/PI_DataSet.txt", r"data/INI_DataSet.txt", r"data/NRTI_DataSet.txt", r"data/NNRTI_DataSet.txt"]
 
     for file in files:
-        running_models(file, (file.split("/")[-1].strip(".txt") + "_OneHot_vs_Internal_results.csv"))
+        running_models(file, "output/" + (file.split("/")[-1].strip(".txt") + "_OneHot_vs_Internal_results.csv"))
 
 if __name__ == '__main__':
     main()
