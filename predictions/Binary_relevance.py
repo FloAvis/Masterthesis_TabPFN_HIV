d733677d842308dcd952d9b6bf0f1a2b188ae868
FloAvis <152602773+FloAvis@users.noreply.github.com>
2025-08-29 15:46:42 +0200

    debugging data saving, testing labels probably saved wrongly

--- predictions/Binary_relevance.py
+++ predictions/Binary_relevance.py
@@ -122,7 +122,7 @@ def main():
                 k += 1
 
             #y_pred_df["kFolds"] = kfolds
-
+            """
             y_test = np.zeros((y_pred[0].shape[0], Y.shape[1]))
 
             t = 0
@@ -133,18 +133,18 @@ def main():
                     for j in range(Y.shape[1]):
                         y_test[t, j] = Y.iloc[i, j]
                     t += 1
+            
+            """
 
+            #y_test_df = pd.DataFrame(y_test, columns=drugs)
 
-
-            y_test_df = pd.DataFrame(y_test, columns=drugs)
-
-            utils.save_multilabel(y_pred_df, y_test_df, k_folds=kfolds, label=(
+            utils.save_multilabel(y_pred_df, Y, k_folds=kfolds, label=(
                         file.split("/")[-1].split("_")[0] + "_results/" + file.split("/")[-1].split("_")[
                     0] + "_Binary_Relevance_"+ str(folds) + "_fold_MOC_prediction"))
 
 
 
-            utils.save_multilabel_proba(y_pred, y_test_df, k_folds=kfolds, label=(
+            utils.save_multilabel_proba(y_pred, Y, k_folds=kfolds, label=(
                     file.split("/")[-1].split("_")[0] + "_results/" + file.split("/")[-1].split("_")[
                 0] + "_Binary_Relevance_probabilities_"+ str(folds) + "_fold_MOC_prediction"))
 
